452ff4edd69ce3381481bbae2015197c06481bb9
Foo Lim <foo.lim@vungle.com>
2015-01-30 10:34:22 -0800

    Evolutions & configuration for a localhost mysql db as backend

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,24 +1,49 @@
 # --- !Ups
 
 CREATE TABLE zookeepers (
-  name VARCHAR,
-  host VARCHAR,
-  port INT,
-  statusId LONG,
-  groupId LONG,
-  PRIMARY KEY (name)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  host VARCHAR(255) NOT NULL,
+  port INT NOT NULL,
+  statusId INT NOT NULL,
+  groupId INT NOT NULL,
+  chroot VARCHAR(255)
 );
 
 CREATE TABLE groups (
-  id LONG,
-  name VARCHAR,
+  id INT,
+  name VARCHAR(255),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE status (
-  id LONG,
-  name VARCHAR,
+  id INT,
+  name VARCHAR(255),
   PRIMARY KEY (id)
+);
+
+CREATE TABLE offsetHistory (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  zookeeperId INT,
+  topic VARCHAR(255),
+  FOREIGN KEY zkId (zookeeperId) REFERENCES zookeepers(id) ON DELETE CASCADE,
+  UNIQUE zkTopic (zookeeperId, topic)
+);
+
+CREATE TABLE offsetPoints (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  consumerGroup VARCHAR(255),
+  timestamp DATETIME,
+  offsetHistoryId BIGINT,
+  partition INT,
+  offset BIGINT,
+  logSize BIGINT,
+  FOREIGN KEY offhistId (offsetHistoryId) REFERENCES offsetHistory(id) ON DELETE CASCADE
+);
+
+CREATE TABLE settings (
+  key_ VARCHAR(255) PRIMARY KEY,
+  value VARCHAR(255)
 );
 
 INSERT INTO groups (id, name) VALUES (0, 'ALL');
@@ -32,8 +57,14 @@ INSERT INTO status (id, name) VALUES (1, 'CONNECTED');
 INSERT INTO status (id, name) VALUES (2, 'DISCONNECTED');
 INSERT INTO status (id, name) VALUES (3, 'DELETED');
 
+INSERT INTO settings (key_, value) VALUES ('PURGE_SCHEDULE', '0 0 0 ? * SUN *');
+INSERT INTO settings (key_, value) VALUES ('OFFSET_FETCH_INTERVAL', '30');
+
 # --- !Downs
 
-DROP TABLE IF EXISTS zookeepers;
-DROP TABLE IF EXISTS groups;
+DROP TABLE IF EXISTS settings;
+DROP TABLE IF EXISTS offsetPoints;
+DROP TABLE IF EXISTS offsetHistory;
 DROP TABLE IF EXISTS status;
+DROP TABLE IF EXISTS groups;
+DROP TABLE IF EXISTS zookeepers;
--- conf/evolutions/default/2.sql
+++
@@ -1,7 +0,0 @@
-# --- !Ups
-
-ALTER TABLE zookeepers ADD COLUMN chroot VARCHAR;
-
-# --- !Downs
-
-ALTER TABLE zookeepers DROP COLUMN chroot;
